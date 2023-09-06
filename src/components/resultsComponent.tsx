fc74f7f71a9a7c93902ef13ccafb712573a5796f
kq333 <afugowski@yandex.com>
2023-09-06 22:18:09 +0200

    added props

--- src/components/resultsComponent.tsx
+++ src/components/resultsComponent.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 import backgroundImage from '../assets/undraw_winners_ao2o 2.svg'
 
-export const ResultComponent = () => {
+interface Props {
+  quizResult: number
+  reloadQuize: () => void
+}
+
+export const ResultComponent: React.FC<Props> = (props) => {
+  const { quizResult } = props
+
+  const reloadQuiz = () => {
+    props.reloadQuize()
+  }
+
   return (
     <div className="mx-auto flex-col justify-center">
       <h2 className="py-4 text-4xl font-bold leading-10 text-white">
@@ -24,12 +35,15 @@ export const ResultComponent = () => {
 
         <p className="flex justify-center pb-[51px] items-center text-sm md:text-lg">
           You got{' '}
-          <span className="text-[#6FCF97] text-4xl font-bold px-1 ">4</span>
+          <span className="text-[#6FCF97] text-4xl font-bold px-1 ">
+            {quizResult}
+          </span>
           correct answers
         </p>
 
         <div className="flex justify-center">
           <button
+            onClick={() => reloadQuiz()}
             type="button"
             className="w-[209px] h-[62px] text-lg  rounded-xl  border border-[#1D355D] hover:border-opacity-75 hover:text-opacity-75 text-[#1D355D]"
           >
