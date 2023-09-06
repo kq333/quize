363000b7b00d7dcd441d09fa8a6349b9e7c5f9fd
kq333 <afugowski@yandex.com>
2023-09-06 20:37:09 +0200

    finished logic in pickanswere component

--- src/components/pickAnswer.tsx
+++ src/components/pickAnswer.tsx
@@ -9,17 +9,28 @@ interface Props {
   correctAnswer: string
   answers: string[]
   flags: boolean
+  updateQuiz: () => void
 }
 
 export const PickAnsware: React.FC<Props> = (props) => {
-  const [isChecked, useIsChecked] = useState(false)
+  const [isAnswere, setIsAnswere] = useState<string>('')
+  const [checkAnswere, setCheckAnswere] = useState<boolean>(false)
 
-  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
-
-  const icons = isChecked ? deleteIcon : checkIcon
+  const alphabet = 'ABCD'.split('')
 
   const { question, flagIcon, correctAnswer, answers, flags } =
     props.fetchedData
+
+  const buttonEvents = (elem: string) => {
+    setIsAnswere(elem)
+    setCheckAnswere(!checkAnswere)
+  }
+
+  const buttonNext = () => {
+    props.updateQuiz()
+    setIsAnswere('')
+    setCheckAnswere(false)
+  }
 
   return (
     <div>
@@ -47,7 +58,7 @@ export const PickAnsware: React.FC<Props> = (props) => {
             </div>
           )}
 
-          <div className="py-8 px-8">
+          <div className="py-10 px-8">
             <h2 className="text-[#2F527B] md:text-2xl text-lg font-bold pb-8">
               {question}
             </h2>
@@ -56,15 +67,69 @@ export const PickAnsware: React.FC<Props> = (props) => {
               {Array.isArray(answers) &&
                 answers.map((elem, index) => (
                   <li
-                    className="h-[56px] md:w-[400px] max-w-[400px] border rounded-xl border-[#6066D0] flex items-center cursor-pointer hover:bg-[#F9A826] hover:text-white text-[#2F527B] mt-8"
+                    className={`h-[56px] md:w-[400px] max-w-[400px] border rounded-xl  flex items-center cursor-pointer mt-8 hover:bg-[#F9A826] bg-[#ffffff] border-[#6066D0] hover:border-[#F9A826] hover:text-white
+
+                    ${
+                      isAnswere === elem && !checkAnswere
+                        ? 'text-white'
+                        : 'text-[#2F527B]'
+                    }
+
+                    ${
+                      correctAnswer === elem && checkAnswere
+                        ? 'bg-[#60BF88]'
+                        : ''
+                    }
+                  ${correctAnswer === elem && checkAnswere ? 'text-white' : ''}
+                ${
+                  correctAnswer === elem && checkAnswere
+                    ? 'border-[#60BF88]'
+                    : ''
+                }
+
+                ${
+                  elem === isAnswere && elem !== correctAnswer
+                    ? 'bg-red-400'
+                    : ''
+                }
+
+                ${
+                  elem === isAnswere && elem !== correctAnswer
+                    ? 'text-white'
+                    : ''
+                }
+
+                ${
+                  elem === isAnswere && elem !== correctAnswer
+                    ? 'border-red-400'
+                    : ''
+                }
+                ${checkAnswere ? 'pointer-events-none' : ''}
+                    `}
                     key={index}
+                    onClick={() => buttonEvents(elem)}
                   >
                     <span className="px-4">{alphabet[index]}</span>
                     <span className="flex-grow md:text-lg text-xs font-medium">
                       {elem}
                     </span>
-                    <span className="md:px-4 px-3">
-                      <img src={icons} alt="icon" />
+                    <span
+                      className={`md:px-4 px-3 ${
+                        (correctAnswer === elem && checkAnswere) ||
+                        (elem === isAnswere && checkAnswere)
+                          ? 'block'
+                          : 'hidden'
+                      }`}
+                    >
+                      <img
+                        src={` ${
+                          correctAnswer === elem && checkAnswere
+                            ? checkIcon
+                            : deleteIcon
+                        }
+                         `}
+                        alt="icon"
+                      />
                     </span>
                   </li>
                 ))}
@@ -73,7 +138,9 @@ export const PickAnsware: React.FC<Props> = (props) => {
             <div className="flex justify-end pt-8">
               <button
                 type="button"
-                className="bg-[#F9A826] w-[115px] h-[56px] text-lg text-white rounded-xl hover:opacity-75"
+                className={`bg-[#F9A826] w-[115px] h-[56px] text-lg text-white rounded-xl hover:opacity-75
+                ${isAnswere.length ? 'block' : 'hidden'}`}
+                onClick={() => buttonNext()}
               >
                 Next
               </button>
